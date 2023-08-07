d1279607b5fb223834cc8811cef75315cbcc63f0
Pedro Oriani <pedro.oriani@gmail.com>
2023-08-06 23:22:24 -0300

    tentando arrumar o corretor HUB

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -9,6 +9,8 @@ const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+configDatabase.ssl = false;
+
+//if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
