069778a56f21c8890d8b6e8200d82904550fa1c1
Pedro Oriani <pedro.oriani@gmail.com>
2023-08-07 12:32:52 -0300

    arrumando shorting Url

--- src/controllers/url.controllers.js
+++ src/controllers/url.controllers.js
@@ -3,17 +3,17 @@ import { db } from "../database/database.connection.js";
 
 export async function shortingUrl(req, res){
 
-    const url = req.body.url;
+    const { url } = req.body;
     
     const user = res.locals.user;
 
-    const shortUrl = nanoid();
+    const shortUrl = nanoid(8);
 
     try{
 
         await db.query(`INSERT INTO urls ("userID", url, "shortUrl") VALUES ($1, $2, $3);`, [user.rows[0].id, url, shortUrl]);
 
-        const postedUrl = await db.query(`SELECT * FROM url WHERE "shortUrl"=$1;`, [shortUrl]);
+        const postedUrl = await db.query(`SELECT * FROM urls WHERE "shortUrl"=$1;`, [shortUrl]);
 
         res.status(200).send({id: postedUrl.rows[0].id, shortUrl});
 
