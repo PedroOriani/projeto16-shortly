3835bb2bf5999b1b418230751ed87254b9ce241f
Pedro Sebinelli de Godoy Oriani <TRPEDORI@traumec.com.br>
2023-08-07 10:17:45 -0300

    corrigindo cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import router from "./routes/index.routes.js";
 const app = express();
 
 app.use(express.json())
-app.use(cors);
+app.use(cors());
 app.use(router);
 
 const port = process.env.PORT || 5000
